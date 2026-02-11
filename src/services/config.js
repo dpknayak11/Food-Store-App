5aa3a3d740866b4894dfa41353591359bae0227a
Deepak nayak <dpknayak111@gmail.com>
2026-02-11 19:15:34 +0530

    update url

--- src/services/config.js
+++ src/services/config.js
@@ -1,4 +1,4 @@
 // config.js
     
 // export const BASE_URL = "http://localhost:5000/api/v1";
-export const BASE_URL = "https://food-store-backend-six.vercel.app/api/v1";
+export const BASE_URL = "https://food-store-backend-dpk.vercel.app/api/v1";
